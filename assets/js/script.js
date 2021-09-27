14e6882b2cb902cf869f17cbe90c5d577ca046d6
Jillium <86424914+Jillium@users.noreply.github.com>
2021-09-27 11:44:41 -0600

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,31 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 //create task function
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
 
     event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name= 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
+    console.log(taskTypeInput);
 
 
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.classname = "task-info";
+    //add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-    console.log(event);
+    
 }
 
 
