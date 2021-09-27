b48193d2d3dc1843155a252fda88f032895cac8c
Jillium <86424914+Jillium@users.noreply.github.com>
2021-09-27 11:24:19 -0600

    form event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
 // Assignment 
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 
 //create task function
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+    console.log(event);
 }
 
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
